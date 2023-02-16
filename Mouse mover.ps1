2ad5b167e24c347cd28647a43d3fb70f5ec2545c
introoox <125462829+introoox@users.noreply.github.com>
2023-02-16 13:58:00 +0100

    Add files via upload

---
+++ Mouse mover.ps1
@@ -0,0 +1,13 @@
+﻿Add-Type -AssemblyName System.Windows.Forms
+[System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point(850, 500)
+$SendMouseClick::mouse_event(0x00000002, 0, 0, 0, 0);
+$SendMouseClick::mouse_event(0x00000004, 0, 0, 0, 0);
+
+$Mouse=@' 
+[DllImport("user32.dll",CharSet=CharSet.Auto, CallingConvention=CallingConvention.StdCall)]
+public static extern void mouse_event(long dwFlags, long dx, long dy, long cButtons, long dwExtraInfo);
+'@ 
+$SendMouseClick = Add-Type -memberDefinition $Mouse -name "Win32MouseEventNew" -namespace Win32Functions -passThru
+
+
+ 
